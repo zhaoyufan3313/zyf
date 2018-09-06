ac6bbe6a00aed83a28ea090071235ba65687095a
zyf <915543068@qq.com>
2018-09-06 10:23:54 +0800

    提交了一个文件

---
+++ src/main/java/com/mr/git/Test2.java
@@ -0,0 +1,10 @@
+package com.mr.git;
+
+/**
+ * Created by Administrator on 2018/9/6.
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("我是你爹");
+    }
+}
